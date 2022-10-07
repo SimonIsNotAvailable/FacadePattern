2e0b45d2a50204509195978cd786dec93125cc95
SimonIsNotAvailable <polushkinsn@gmail.com>
2022-10-07 16:01:25 +0500

    Binary sum and mult to BinOps added

--- src/BinOps.java
+++ src/BinOps.java
@@ -1,15 +1,13 @@
 public class BinOps {
     public String sum(String a, String b) {
-    /*Переведите числа из двоичной записи в
-      строках в int, сложите и переведите
-      обратно в двоичную запись*/
-        return null;
+        int arg1 = Integer.parseInt(a, 2);
+        int arg0 = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(arg0 + arg1);
     }
 
     public String mult(String a, String b) {
-    /*Переведите числа из двоичной записи в
-      строках в int, перемножьте и переведите
-      обратно в двоичную запись*/
-        return null;
+        int arg1 = Integer.parseInt(a, 2);
+        int arg0 = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(arg1 * arg0);
     }
 }
